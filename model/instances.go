d6026a485ad825520b3b9555c711b583c0bc92c4
Simon Shillaker <sshillaker@scaleway.com>
2024-07-11 15:36:32 +0200

    Clean-up

--- model/instances.go
+++ model/instances.go
@@ -2,72 +2,6 @@ package model
 
 import "fmt"
 
-// ----- Docs -----
-// Base server types: https://www.scaleway.com/en/docs/compute/instances/reference-content/instances-datasheet/#developement-and-general-purpose-instances
-// Instances table: https://www.scaleway.com/en/pricing/?tags=compute
-
-const (
-	DefaultInstanceHddGib = 20
-
-	InstancePlay2Pico  = "play2-pico"
-	InstancePlay2Nano  = "play2-nano"
-	InstancePlay2Micro = "play2-micro"
-
-	InstancePro2Xxs = "pro2-xxs"
-	InstancePro2Xs  = "pro2-xs"
-	InstancePro2S   = "pro2-s"
-	InstancePro2M   = "pro2-m"
-	InstancePro2L   = "pro2-l"
-
-	InstanceDev1S  = "dev1-s"
-	InstanceDev1M  = "dev1-m"
-	InstanceDev1L  = "dev1-l"
-	InstanceDev1Xl = "dev1-xl"
-
-	InstanceGp1Xs = "gp1-xs"
-	InstanceGp1S  = "gp1-s"
-	InstanceGp1M  = "gp1-m"
-	InstanceGp1L  = "gp1-l"
-	InstanceGp1Xl = "gp1-xl"
-
-	InstancePop22c = "pop2-2c-8g"
-	InstancePop24c = "pop2-4c-16g"
-	InstancePop28c = "pop2-8c-32g"
-	InstancePop16c = "pop2-16c-64g"
-	InstancePop32c = "pop2-32c-128g"
-	InstancePop64c = "pop2-64c-256g"
-
-	InstanceEnt1Xxs = "ent1-xxs"
-	InstanceEnt1Xs  = "ent1-xs"
-	InstanceEnt1S   = "ent1-s"
-	InstanceEnt1M   = "ent1-xm"
-	InstanceEnt1L   = "ent1-l"
-	InstanceEnt1Xl  = "ent1-xl"
-	InstanceEnt1Xxl = "ent1-2xl"
-
-	InstancePop2HM2C  = "pop2-hm-2c-16g"
-	InstancePop2HM4C  = "pop2-hm-4c-32g"
-	InstancePop2HM8C  = "pop2-hm-8c-64g"
-	InstancePop2HM16C = "pop2-hm-16c-128g"
-	InstancePop2HM32C = "pop2-hm-32c-256g"
-	InstancePop2HM64C = "pop2-hm-64c-512g"
-
-	InstancePop2HC2C  = "pop2-hc-2c-4g"
-	InstancePop2HC4C  = "pop2-hc-4c-8g"
-	InstancePop2HC8C  = "pop2-hc-8c-16g"
-	InstancePop2HC16C = "pop2-hc-16c-32g"
-	InstancePop2HC32C = "pop2-hc-32c-64g"
-	InstancePop2HC64C = "pop2-hc-64c-128g"
-
-	InstanceStardust1 = "stardust1"
-
-	InstanceCopArm2C8G    = "coparm1-2c-8g"
-	InstanceCopArm4C16G   = "coparm1-4c-16g"
-	InstanceCopArm8C32G   = "coparm1-8c-32g"
-	InstanceCopArm16C64G  = "coparm1-16c-64g"
-	InstanceCopArm32C128G = "coparm1-32c-128g"
-)
-
 // An identifier for an instance type
 type Instance struct {
 	Type        string
@@ -362,16 +296,6 @@ var BaseCopArm1Host = Server{
 func InstanceToString(instanceBase VirtualMachine) string {
 	cpuName := instanceBase.Server.Cpus[0].Name
 	return fmt.Sprintf("%v vCPU, %v GiB, %v CPU", instanceBase.VCpus, instanceBase.RamGiB, cpuName)
-}
-
-func buildInstanceBase(baseServer Server, nVcpu uint32, ramGiB uint32) VirtualMachine {
-	server := baseServer
-
-	return VirtualMachine{
-		VCpus:  nVcpu,
-		RamGiB: ramGiB,
-		Server: server,
-	}
 }
 
 var BaseInstanceServers = []Server{
